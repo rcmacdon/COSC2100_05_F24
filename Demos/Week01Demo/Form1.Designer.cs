0141b85cb06b51270cb552885fb94a91fb5ba0d9
rcmacdon <clint.macdonald@durhamcollege.ca>
2024-09-10 19:24:50 -0400

    Week 01 Demo Commit trial 2

---
+++ Demos/Week01Demo/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Week01Demo
+{
+    partial class frmMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHelloWorld = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHelloWorld
+            // 
+            this.btnHelloWorld.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHelloWorld.ForeColor = System.Drawing.Color.Maroon;
+            this.btnHelloWorld.Location = new System.Drawing.Point(171, 74);
+            this.btnHelloWorld.Name = "btnHelloWorld";
+            this.btnHelloWorld.Size = new System.Drawing.Size(439, 175);
+            this.btnHelloWorld.TabIndex = 0;
+            this.btnHelloWorld.Text = "Hello World";
+            this.btnHelloWorld.UseVisualStyleBackColor = true;
+            this.btnHelloWorld.Click += new System.EventHandler(this.btnHelloWorld_Click);
+            // 
+            // frmMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(210)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHelloWorld);
+            this.Name = "frmMainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clint\'s First Application";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHelloWorld;
+    }
+}
+
---
+++ Demos/Week01Demo/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Week01Demo
+{
+    public partial class frmMainForm : Form
+    {
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHelloWorld_Click(object sender, EventArgs e)
+        {
+            if ( MessageBox.Show("Hi Clint", "Clint was Here",MessageBoxButtons.YesNoCancel) == DialogResult.Yes)
+            {
+                btnHelloWorld.Text = "Yeah!";
+            };
+
+        }
+    }
+}
